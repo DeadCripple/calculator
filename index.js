f19a220d42c0a6f15f97f2f42666271cca3a92a0
DeadCripple <4ndr.krn@gmail.com>
2020-02-14 21:08:21 +0600

    Третье задание

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-let money = +prompt("Ваш бюджет на месяц?", '');
-let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let money;
+let time;
+
+function start() {
+  money = +prompt("Ваш бюджет на месяц?", '');
+  time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+  while (isNaN(money) || money == "" || money == null) {
+    alert('Вы ввели некорректное значение бюджета. Пожалуйста, повторите ввод.', '');
+    money = +prompt("Ваш бюджет на месяц?", "");
+  }
+}
+start();
 
 // Тело программы, куда записывается основная информация
 let appData = {
@@ -8,38 +19,71 @@ let appData = {
   expenses: {},
   optionalExpenses: {},
   income: [],
-  savings: false
+  savings: true
 };
 
-/* Цикл с вопросами о статьях расхода и 
+/* Функция с вопросами о статьях расхода и 
 последующей записью в основной объект программы */
-for (let i = 0; i < 2; i++) {
-  let a = prompt('Введите обязательную статью расходов в этом месяце', '');
-  let b = prompt('Во сколько обойдется?', '');
-  
-  if (typeof(a) === 'string' && typeof(a) != null && 
-  typeof(b) != null && a != '' && b != '' && a.length < 50) {
-    // Запись результатов вопросов в объект expenses объекта appData
-    appData.expenses[a] = b;
-    console.log('done');
-  } else {
-    // Повторяем итерацию, если будет ошибка в проверке
-    i--;
-    console.log('error');
+function chooseExpenses() {
+  for (let i = 0; i < 2; i++) {
+    let a = prompt('Введите обязательную статью расходов в этом месяце', '');
+    let b = prompt('Во сколько обойдется?', '');
+
+    if (typeof (a) === 'string' && typeof (a) != null &&
+      typeof (b) != null && a != '' && b != '' && a.length < 50) {
+      // Запись результатов вопросов в объект expenses объекта appData
+      appData.expenses[a] = b;
+      console.log('done');
+    } else {
+      // Повторяем итерацию, если будет ошибка в проверке
+      i--;
+      console.log('error');
+    }
   }
 }
+chooseExpenses();
 
 // Вычисление бюджета на день и добавление результата в объект appData
-appData.moneyPerDay = appData.budget / 30;
-alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+function detectDayBudget() {
+  appData.moneyPerDay = (appData.budget / 30).toFixed();
+
+  alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+}
+detectDayBudget()
 
 // Определение уровня достатка
-if (appData.moneyPerDay < 100) {
+function detectLevel() {
+  if (appData.moneyPerDay < 100) {
     console.log('У Вас минимальный уровень достатка');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
     console.log('У Вас средний уровень достатка');
-} else if (appData.moneyPerDay > 2000) {
+  } else if (appData.moneyPerDay > 2000) {
     console.log('У Вас высокий уровень достатка');
-} else {
+  } else {
     console.log('Произошла ошибка');
+  }
 }
+detectLevel();
+
+
+// Вычисление дохода по депозиту
+function checkSavings() {
+  if (appData.savings == true) {
+    let save = +prompt('Какова сумма накоплений?', '');
+    let percent = +prompt('Под какой процент?', '');
+
+    appData.monthIncome = (save / 100 / 12 * percent).toFixed();
+    alert('Доход из вашего дипозита: ' + appData.monthIncome);
+  }
+}
+checkSavings();
+
+// Определение необязательных расходов и запись в объект appData
+function chooseOptExpenses() {
+  for (let i = 1; i <= 3; i++) {
+    let questionOptExpenses = prompt('Статья необязательных расходов?');
+    appData.optionalExpenses[i] = questionOptExpenses;
+    console.log(appData.optionalExpenses);
+  }
+}
+chooseOptExpenses();
